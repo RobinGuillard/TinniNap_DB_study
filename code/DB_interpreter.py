885c3779ea8564ece741c54c9ba88cbab759564b
Robin Guillard <robin.guillard@gmail.com>
2022-10-06 16:38:46 +0200

    getting_means_stds_medians

--- code/DB_interpreter.py
+++ code/DB_interpreter.py
@@ -1,5 +1,7 @@
 import os
 import pandas as pd
+import config
+import numpy as np
 
 def split_database_col(df,col):
     """ Generates n smaller df from an initial df for each n different values of column col. Must be used for
@@ -21,6 +23,28 @@ def split_database_col(df,col):
         dic_df[elm] = df[df[col] == elm]
     return dic_df
 
+def get_mean_std_num_cols(dic_df, num_cols):
+    """ Generates n smaller df from an initial df for each n different values of column col. Must be used for
+        categorical col
+
+            Parameters
+            ----------
+            dic_df : python dictionnary
+                a dictionnary where the keys are the split col values and the values are the partial databases
+            num_cols: list of string,
+                Names of the numerical continuous columns of the dataset imported from config
+            Returns
+
+            """
+    dic_cols = {}
+    for col in num_cols:
+        dic_cols[col] = { "order_vals":[],"means":[], "stds":[], "medians":[]}
+        for X in list(dic_df.keys()):
+            dic_cols[col]["order_vals"].append(X)
+            dic_cols[col]["means"].append(np.mean(dic_df[X][col]))
+            dic_cols[col]["stds"].append(np.std(dic_df[X][col]))
+            dic_cols[col]["medians"].append(np.median(dic_df[X][col]))
+    return dic_cols
 
 
 
@@ -29,5 +53,11 @@ if __name__ == "__main__":
     FILENAME = os.path.join(DIRECTORY, "sarah_michiels_v2.csv")
     # Avoiding path issues related to different OS
     data = pd.read_csv(FILENAME, sep=",", encoding="latin1")
-    dic_df = split_database_col(data, "Female")
+    dic_df = split_database_col(data, "InflNap")
     print(list(data.keys()))
+    num_cols = config.SM_num_cols
+    print(num_cols)
+    dic_cols = get_mean_std_num_cols(dic_df, num_cols)
+    for elm in dic_cols:
+        print(elm)
+        print(dic_cols[elm])
--- test/test_DB_interpreter.py
+++ test/test_DB_interpreter.py
@@ -5,7 +5,7 @@ import numpy as np
 
 sys.path.insert(0, 'D:/Documents/Thèse EDISCE/TinniNap_DB_study/code')
 
-from DB_interpreter import split_database_col
+from DB_interpreter import split_database_col, get_mean_std_num_cols
 
 def test_split_database_col():
 
@@ -20,3 +20,19 @@ def test_split_database_col():
     assert len(list(dic_df.keys())) == 2
     assert list(dic_df.keys()) == [1,2]
     assert list(dic_df[1]["B"]) == [12,17]
+
+def test_get_mean_std_num_cols():
+    dict1 = {"A": 1, "B": 12, "C": 14}
+    dict2 = {"A": 1, "B": 17, "C": 12}
+    dict3 = {"A": 2, "B": 11, "C": 14}
+    dict4 = {"A": 2, "B": 11, "C": 14}
+    dictList = [dict1, dict2, dict3, dict4]
+    myDf = pd.DataFrame(dictList)
+
+    dic_df = split_database_col(myDf, "A")
+    dic_cols = get_mean_std_num_cols(dic_df, ["B", "C"])
+
+    assert list(dic_cols.keys()) == ["B", "C"]
+    assert dic_cols[list(dic_cols.keys())[0]]["means"] == [14.5, 11]
+    assert dic_cols[list(dic_cols.keys())[1]]["stds"] == [np.std([14, 12]), 0]
+
